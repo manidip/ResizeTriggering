277bbaf177723d11f48cd7bbd1f36d8acdd7eedc
addUI <info@add-ui.com>
2016-08-22 13:39:02 -0400

    2.0.0

--- src/ResizeTriggering.js
+++ src/ResizeTriggering.js
@@ -1,29 +1,76 @@
-(function($){
-  var $listening = $(), lastSize = [], timerID = null;
-  $.ResizeTriggeringInterval = 16;
-  $.fn.resizeTriggering = function(){
-    $listening = $listening.add(this);
-    checkForResize();
+jQuery.ResizeTriggering = (function($){
+  function ResizeTriggering(selector, direction){
+    ResizeTriggering.start(selector, direction);
     return this;
   };
-  function checkForResize(){
-    clearTimeout(timerID);
-    $listening.each(function(i, el){
+  ResizeTriggering._interval = 16;
+  Object.defineProperty(ResizeTriggering, "interval", {
+    get: function(){
+      return ResizeTriggering._interval;
+    },
+    set: function(ms){
+      clearInterval(ResizeTriggering.intervalID);
+      ResizeTriggering._interval = ms;
+      ResizeTriggering.intervalID = window.setInterval(ResizeTriggering._check, ResizeTriggering._interval);
+    }
+  });
+  ResizeTriggering._elements = $();
+  ResizeTriggering._directions = [];
+  ResizeTriggering._sizes = [];
+  ResizeTriggering._check = function(){
+    ResizeTriggering._elements.each(function(i, el){
       var $el = $(el);
-      var size = {
-        width: $el.width(),
-        height: $el.height()
+      var s = {
+        w: $el.width(),
+        h: $el.height()
       };
-      var last = lastSize[i];
+      var d = ResizeTriggering._directions[i];
+      var o = ResizeTriggering._sizes[i];
       if(
-        !last ||
-        last.width != size.width ||
-        last.height != size.height
-      ){
-        lastSize[i] = size;
-        $el.trigger("resize");
+        (
+          d == "both" &&
+          (
+            o.w != s.w ||
+            o.h != s.h
+          )
+        ) ||
+        (
+          d == "width" &&
+          o.w != s.w
+        ) ||
+        (
+          d == "height" &&
+          o.h != s.h
+        )
+      ) $el.trigger("resize");
+    });
+  };
+  ResizeTriggering.intervalID = window.setInterval(ResizeTriggering._check, ResizeTriggering._interval);
+  ResizeTriggering.start = function(selector, direction){
+    var $el = $(selector);
+    ResizeTriggering._elements = ResizeTriggering._elements.add($el);
+    ResizeTriggering._directions.push(direction || "both");
+    ResizeTriggering._sizes.push({
+      w: $el.width(),
+      h: $el.height()
+    });
+  };
+  ResizeTriggering.stop = function(selector){
+    var index = -1;
+    var $el = $(selector);
+    ResizeTriggering._elements.each(function(i, el){
+      if($(el) == $el){
+        index = i;
       }
     });
-    setTimeout(checkForResize, $.ResizeTriggeringInterval);
+    if(index > -1){
+      
+    }
   };
+  ResizeTriggering.version = "2.0.0";
+  $.fn.resizeTriggering = function(direction){
+    ResizeTriggering.start(this, direction);
+    return this;
+  };
+  return ResizeTriggering;
 })(jQuery);
